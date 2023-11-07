b65459f9bb4f9af756160626365e769ed9dc254c
Sasibru <sander.simonsen.brustad@gmail.com>
2023-11-07 20:17:51 +0100

    Add project files.

---
+++ CardGameApp/PlayingCard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameApp
+{
+	public class PlayingCard
+	{
+		public enum PlayingCards
+		{
+			None,
+			Ace,
+			Two,
+			Three,
+			Four,
+			Five,
+			Six,
+			Seven,
+			Eight,
+			Nine,
+			Ten,
+			Jack,
+			Queen,
+			King
+		}
+
+		public enum PlayingCardsType
+		{
+			Clubs,
+			Diamonds,
+			Hearts,
+			Spades
+		}
+
+		public bool isCardVisible = false;
+
+	}
+}
