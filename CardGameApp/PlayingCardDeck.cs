9854156d63ebbadda19ba50294b2c2f0a0675529
Sasibru <sander.simonsen.brustad@gmail.com>
2023-11-09 08:44:11 +0100

    Improve ContainsAllCards method

--- CardGameApp/PlayingCardDeck.cs
+++ CardGameApp/PlayingCardDeck.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -35,12 +36,22 @@ namespace CardGameApp
 		public bool ContainsAllCards()
 		{
 			CreateDeck();
-			int clubsCount = deckList.Count(card => card.Suit == PlayingCard.SuitEnums.Clubs);
-			int spadesCount = deckList.Count(card => card.Suit == PlayingCard.SuitEnums.Spades);
-			int heartsCount = deckList.Count(card => card.Suit == PlayingCard.SuitEnums.Hearts);
-			int diamondsCount = deckList.Count(card => card.Suit == PlayingCard.SuitEnums.Diamonds);
+			//int clubsCount = deckList.Count(card => card.Suit == PlayingCard.SuitEnums.Clubs);
+			//int spadesCount = deckList.Count(card => card.Suit == PlayingCard.SuitEnums.Spades);
+			//int heartsCount = deckList.Count(card => card.Suit == PlayingCard.SuitEnums.Hearts);
+			//int diamondsCount = deckList.Count(card => card.Suit == PlayingCard.SuitEnums.Diamonds);
 
-			return (clubsCount == 13 || spadesCount == 13 || heartsCount == 13 || diamondsCount == 13);
+			//return (clubsCount == 13 || spadesCount == 13 || heartsCount == 13 || diamondsCount == 13);
+
+			if (deckList.Count != deckList.Distinct().Count() && deckList.Count == 52)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+			
 		}
 
 	}
